15d66a6850689ab3b7adf5bbfc0f345f8906aa69
jatwing <jatwing@pm.me>
2021-11-10 16:56:20 +0800

    6. add notifications, projects, rankings and translations apis.

--- src/app.js
+++ src/app.js
@@ -21,9 +21,59 @@ app.get('/author', async (req, res) => {
     return;
   }
   const author = await database.collection('authors').findOne({
-    name: 'jatwing',
+    id: 'jatwing',
   });
   res.send(author);
+});
+
+app.get('/notifications', async (req, res) => {
+  const database = await getDatabase();
+  if (database instanceof Error) {
+    res.status(500).send(database.toString());
+    return;
+  }
+  const notifications = await database
+    .collection('notifications')
+    .find({
+      isActive: true,
+    })
+    .toArray();
+  res.send(notifications);
+});
+
+app.get('/project', async (req, res) => {
+  const database = await getDatabase();
+  if (database instanceof Error) {
+    res.status(500).send(database.toString());
+    return;
+  }
+  const project = await database.collection('authors').findOne({
+    id: 'react-note',
+  });
+  res.send(project);
+});
+
+app.get('/rankings', async (req, res) => {
+  const database = await getDatabase();
+  if (database instanceof Error) {
+    res.status(500).send(database.toString());
+    return;
+  }
+  const rankings = await database.collection('rankings').find().toArray();
+  res.send(rankings);
+});
+
+app.get('/translations', async (req, res) => {
+  const database = await getDatabase();
+  if (database instanceof Error) {
+    res.status(500).send(database.toString());
+    return;
+  }
+  const translations = await database
+    .collection('translations')
+    .find()
+    .toArray();
+  res.send(translations);
 });
 
 module.exports = app;
